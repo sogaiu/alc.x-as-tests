81e7c32b14c1c09a49e49d9f7709c47ffabc1abf
sogaiu <983021772@users.noreply.github.com>
2020-07-29 06:33:10 +0900

    Don't use temp file

--- src/alc/x_as_tests/impl/validate.clj
+++ src/alc/x_as_tests/impl/validate.clj
@@ -4,12 +4,7 @@
 
 (defn check-source
   [src]
-  ;; XXX: would like to avoid temp file...
-  ;; XXX: not sure this should always be "clj"
-  (let [file-for-clj-kondo (java.io.File/createTempFile "alcxat" ".clj")
-        _ (spit file-for-clj-kondo src)
-        _ (.deleteOnExit file-for-clj-kondo)
-        path (.getAbsolutePath file-for-clj-kondo)
-        results-map (cc/run! {:lint [path]})
+  (let [results-map (with-in-str src
+                      (cc/run! {:lint ["-"]}))
         num-errs (get-in results-map [:summary :error])]
     (= 0 num-errs)))
