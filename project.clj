66984b3cd25f9c83a624083730bcf7e3c8ec14f9
sogaiu <983021772@users.noreply.github.com>
2020-07-29 08:13:25 +0900

    Tweak deps and build

--- project.clj
+++ project.clj
@@ -5,13 +5,21 @@
   :dependencies [[carocad/parcera "0.11.1"]
                  [clj-kondo "2020.07.26"]
                  [org.antlr/antlr4-runtime "4.7.1"]
-                 [org.clojure/clojure "1.10.2-alpha1"]]
+                 ;; parcera appears to need >= 1.10.x
+                 [org.clojure/clojure "1.10.1"]]
   :profiles {:socket-repl
              {:jvm-opts
               ["-Dclojure.server.repl={:port 8235
                                        :accept clojure.core.server/repl}"]}
-             :native-image {:dependencies [[borkdude/clj-reflector-graal-java11-fix "0.0.1-graalvm-20.1.0"]
-                                           [borkdude/sci.impl.reflector "0.0.1-java11"]]}
+             ;; see script/compile
+             :clojure-1.10.2-alpha1
+             {:dependencies [[org.clojure/clojure "1.10.2-alpha1"]]}
+             ;; see script/compile
+             :native-image
+             {:dependencies
+              [[borkdude/clj-reflector-graal-java11-fix "0.0.1-graalvm-20.1.0"]
+               [borkdude/sci.impl.reflector "0.0.1-java11"]]}
+             ;;
              :uberjar {:global-vars {*assert* false}
                        :jvm-opts ["-Dclojure.compiler.direct-linking=true"
                                   "-Dclojure.spec.skip-macros=true"]
